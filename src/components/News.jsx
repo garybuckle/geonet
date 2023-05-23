69d4913fb5a5ef2974ee72c9b3c72293f44ff4e3
Gary Buckle <buckle.g@mac.com>
2023-05-23 12:22:44 +0000

    MB13 News with keys loop

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,8 +1,15 @@
 const News = ({ apiData }) => {
+  apiData.forEach((item) => {
+    for (let key in item) {
+      console.log(`${key}: ${item[key]}`);
+    }
+  });
   return (
     <>
       <div>
         <h2>Shows the News on quakes</h2>
+        {}
+        <ul></ul>
       </div>
     </>
   );
